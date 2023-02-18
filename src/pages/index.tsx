46442289b13a7af8833b2dea8a7946ad2936c3f5
julio victor cabanillas sanchez <juliovictor00@hotmail.com>
2023-02-18 12:42:23 -0500

    Add: trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import { trpc } from "@/utils/trpc";
+
 export default function Home() {
+  const {data, isLoading}= trpc.hello.useQuery({ text: "client" });
+  if(isLoading) return <div>IsLoading</div>
+  if(!data) return <div>Error</div>
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is roundest?</div>
-        <div className="p-2" />
+      <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center">
         <div className="w-16 h-16 bg-red-200"> </div>
         <div className="p-8">VS</div>
