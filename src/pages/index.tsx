eb6cec5901ff25e4b7784b30de4edc04c91040fc
julio victor cabanillas sanchez <juliovictor00@hotmail.com>
2023-02-18 12:15:12 -0500

    Add: basic first layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button className="btn btn-primary">hola mundo</button>
-      </main>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which pokemon is roundest?</div>
+        <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center">
+        <div className="w-16 h-16 bg-red-200"> </div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"> </div>
+      </div>
     </div>
   );
 }
