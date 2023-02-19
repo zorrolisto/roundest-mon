60333782d61dce1b6c7d55f54251ab610517c203
julio victor cabanillas sanchez <juliovictor00@hotmail.com>
2023-02-18 20:01:02 -0500

    Update: props for pokemonOption component

--- src/components/PokemonOption.tsx
+++ src/components/PokemonOption.tsx
@@ -1,25 +1,22 @@
 import Image from "next/image";
 import { Pokemon } from "@/types/Pokemon";
 
-export default function PokemonOption({
-  pokemon,
-  voteForThisPokemon,
-}: {
+const PokemonOption: React.FC<{
   pokemon: Pokemon;
   voteForThisPokemon: () => void;
-}) {
-  return (
-    <div
-      className="w-fit h-fit pb-3 px-3 flex flex-col justify-center items-center rounded-lg border-4 border-base-200 hover:bg-base-200 hover:cursor-pointer"
-      onClick={voteForThisPokemon}
-    >
-      <Image
-        src={String(pokemon.sprite)}
-        width={128}
-        height={128}
-        alt="a-pokemon"
-      />
-      <p className="text-xl capitalize mt-[-1.0rem]">{pokemon.name}</p>
-    </div>
-  );
-}
+}> = (props) => (
+  <div
+    className="w-fit h-fit pb-3 px-3 flex flex-col justify-center items-center rounded-lg border-4 border-base-200 hover:bg-base-200 hover:cursor-pointer"
+    onClick={props.voteForThisPokemon}
+  >
+    <Image
+      src={String(props.pokemon.sprite)}
+      width={128}
+      height={128}
+      alt="a-pokemon"
+    />
+    <p className="text-xl capitalize mt-[-1.0rem]">{props.pokemon.name}</p>
+  </div>
+);
+
+export default PokemonOption;
